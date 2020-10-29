b19bf674f7b544a38e9ae9fcea611f614749fe4b
BrayanJcr <70727399+BrayanJcr@users.noreply.github.com>
2020-10-29 09:21:47 -0500

    Agregar archivos de proyecto.

---
+++ BilleteraElectronicaPrueba/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BilleteraElectronicaPrueba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BilleteraElectronicaPrueba")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d350226-2d1f-4a42-a4e0-aa1f95e3298c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
